547896f64d521c7dcd1a5f622664e002687827c3
veronar <vishakha108@gmail.com>
2020-04-14 10:59:44 +0200

    d00 ex00 complete

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -1,7 +1,25 @@
-// This is a comment 
+// This is a comment
 #include <iostream>
+#include <cstring>
+using namespace std;
 
-int main(){
-		std::cout<<"Hello World\n";
-		return 0;
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+
+	for (int i = 1; i < ac; i++)
+	{
+		std::string arg = av[i];
+		for (unsigned int j = 0; j < arg.length(); j++)
+		{
+			arg[j] = toupper(arg[j]);
+		}
+		cout << arg;
+	}
+	cout << "\n";
+	return (0);
 }

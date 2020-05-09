6efd61f5ed0ea2b5d90cdf197767dd813c547056
veronar <vishakha108@gmail.com>
2020-05-09 15:11:44 +0200

    d01 ex01 complete

---
+++ d01/ex01/ex01.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string *panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
+
+// int main()
+// {
+// 	memoryLeak();
+// 	return (0);
+// }
